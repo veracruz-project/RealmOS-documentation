9a77fd9aed1f1e9026c79010655fdfb12027bf6d
Edmund Grimley Evans <edmund.grimley-evans@arm.com>
2023-09-19 15:41:39 +0100

    httpd/: Update this Hermit example.

--- httpd/src/main.rs
+++ httpd/src/main.rs
@@ -1,13 +1,12 @@
 /// Example is derived from tiny-http example
 /// https://github.com/tiny-http/tiny-http/blob/master/examples/hello-world.rs
+
 #[cfg(target_os = "hermit")]
-use hermit_sys as _;
+use hermit as _;
 
 use tiny_http::{Server, Response};
 
 fn main() {
-	let crab = vec![0xF0_u8, 0x9F_u8, 0xA6_u8, 0x80_u8];
-
 	println!("Starting server on port 8080");
 	let server = Server::http("0.0.0.0:8080").unwrap();
 	println!("Now listening on port 8080");
@@ -18,9 +17,7 @@ fn main() {
 			request.url(),
 			request.headers()
 		);
-	
-		let response = Response::from_string("hello world");
-		request.respond(response);
+		let response = Response::from_string("hello world\n");
+		request.respond(response).unwrap();
 	}
-	
 }
